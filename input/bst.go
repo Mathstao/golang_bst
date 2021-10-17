a7ab2ee4998e0e23758572efb0f8f0a87e542595
Denim Horse <udayatenneti@gmail.com>
2021-10-17 06:59:36 +0000

    parsing file creating bst as parsing

--- input/bst.go
+++ input/bst.go
@@ -3,7 +3,15 @@ package main
 import (
     "fmt"
     "flag"
+    //"io/ioutil"
+    "bufio"
+    "os"
+    "log"
+    "strconv"
+    "text/scanner"
+    "strings"
 )
+
 
 func checkfile(e error) {
     if e != nil {
@@ -66,13 +74,27 @@ func (n *Node) Insert (value int){
     }
 }
     
+func test_tree() *Node {
+    tree := &Node{Value: 100}
+    tree.Insert(53)
+    tree.Insert(203)
+    tree.Insert(19)
+    tree.Insert(76)
+    tree.Insert(150)
+    tree.Insert(310)
+    tree.Insert(7)
+    tree.Insert(24)
+    tree.Insert(88)
+    tree.Insert(276)
+    return tree
+}
+
 
 func (n *Node) Search (value int) bool {
     
     if (n==nil){
         return false
     }
-    
     if (value > n.Value) {
         //MOVE RIGHT, value larger
         return n.Right.Search(value)
@@ -83,6 +105,9 @@ func (n *Node) Search (value int) bool {
 }
 
 func main() {
+    
+    //_GLOBAL_BSTMAP := make(map[int][]int)
+    
     fmt.Println("Running go BST sequential")
     
     /*******   PARSING ARGUMENTS   *******/
@@ -99,22 +124,52 @@ func main() {
     fmt.Println("comp workers:", *comp_workers)
     fmt.Println("input file:", *input_file)
     
-    //data, err := os.ReadFile(*input_file)
-    //check(err)
+    file, err := os.Open(*input_file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    
+    file_scanner := bufio.NewScanner(file)
+    // optionally, resize scanner's capacity for lines over 64K, see next example
+    
+    
+    for file_scanner.Scan() {
+        fmt.Println("new line here:")
+        var s scanner.Scanner
+        s.Init(strings.NewReader(file_scanner.Text()))
+        var newBST bool = true
+        var tree *Node;
+        var converted int;
+        for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+            converted, _ = strconv.Atoi(s.TokenText())
+            if newBST{
+                tree = &Node{Value: converted}
+                newBST = false
+            } else {
+                tree.Insert(converted)
+            }
+            /*fmt.Printf("%d ", converted)
+            if (err != nil){
+                fmt.Printf("%d ", converted)
+                
+            } else {
+                fmt.Printf("%s\n", err)
+                fmt.Printf("%s\n", "FATAL ERROR COULDN'T CONVERT TO INTEGER")
+            }*/
+        }
+        fmt.Println(tree)
+        /*for _, value in range scanner.Text(){
+            fmt.Printf("%d ", strconv.Atoi(value))
+        }*/
+        fmt.Printf("\n")
+        //fmt.Println(scanner.Text()[:])
+        fmt.Println("end of line")
+    }
     
     /***** BINARY SEARCH TREE *****/
-    tree := &Node{Value: 100}
-    tree.Insert(53)
-    tree.Insert(203)
-    tree.Insert(19)
-    tree.Insert(76)
-    tree.Insert(150)
-    tree.Insert(310)
-    tree.Insert(7)
-    tree.Insert(24)
-    tree.Insert(88)
-    tree.Insert(276)
     
+    tree := test_tree()
     fmt.Println(tree.Search(400))
     var test[] int
     var count int = 0
@@ -122,4 +177,16 @@ func main() {
     var hash int = tree.computeHash()
     fmt.Printf("%v\n", test)
     fmt.Printf("hash value: %d\n", hash)
+    
+    
+    //code diagram
+    /*
+    1) read file--> if not end of line, insert into binary tree, compute hash, store hashMapBST: list.append(bst id)
+    2) for each key in hashMapBST: compare equality, separate out into linkage somehow
+    
+    
+    
+    */
+    
+    
 }
