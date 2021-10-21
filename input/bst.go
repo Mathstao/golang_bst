a224ce2ab3efc638d932d0abe5224873603f81c7
Denim Horse <udayatenneti@gmail.com>
2021-10-21 16:53:18 +0000

    part 2 data workers and hash workers

--- input/bst.go
+++ input/bst.go
@@ -11,12 +11,17 @@ import (
     "text/scanner"
     "strings"
     "sync"
+    "time"
 )
+
+
+
+/*****************************************************************************************/
 
 type Job struct {
     tree        *Node
     bst_id      int
-    Name      string
+    Name        string
 }
 
 
@@ -25,12 +30,26 @@ type JobChannel chan Job
 type JobQueue chan chan Job
 
 type Worker struct {
-    ID      int
-    JobChan JobChannel
-    Queue   JobQueue   // shared between all workers and dispatchers.
-    Quit    chan struct{}
-    ReturnQueue chan HashBstID
-    SyncWaitGroup *sync.WaitGroup
+    ID             int
+    JobChan        JobChannel
+    Queue          JobQueue   // shared between all workers and dispatchers.
+    Quit           chan struct{}
+    ReturnQueue    chan HashBstID
+    SyncWaitGroup  *sync.WaitGroup
+    WorkersUpdate  bool
+    Mutex          *sync.Mutex
+    Bst_hashmap    *map[int][]int
+}
+
+type disp struct {
+    Workers            []*Worker  // this is the list of workers that dispatcher tracks
+    WorkChan           JobChannel // client submits a job to this channel
+    Queue              JobQueue   // this is the shared JobPool between the workers
+    ReturnQueue        chan HashBstID
+    SyncWaitGroup      *sync.WaitGroup
+    WorkersUpdate      bool
+    Mutex              *sync.Mutex
+    Bst_hashmap        *map[int][]int
 }
 
 func (wr *Worker) Start() {
@@ -39,14 +58,15 @@ func (wr *Worker) Start() {
             wr.Queue <- wr.JobChan //worker puts its jobChan on queue (channel of channels). wr.Queue populated by dispatcher.process()
             select {
             case job := <-wr.JobChan:
-                //fmt.Println("worker id ", wr.ID, job.bst_id)
-                //fmt.Println("updated return queue ", wr.ID, job.bst_id)
-                //fmt.Println("computed hash ", wr.ID, job.bst_id)
-                
                 var hash int = job.tree.computeHash() //todo: extract below to outside, iterate over list of trees with hash_workers
-                hash_bst_pair := HashBstID{hash: hash, bst_id: job.bst_id}
-                wr.ReturnQueue <- hash_bst_pair
-                
+                if wr.WorkersUpdate {
+                    wr.Mutex.Lock()
+                    (*wr.Bst_hashmap)[hash] = append((*wr.Bst_hashmap)[hash], job.bst_id)
+                    wr.Mutex.Unlock()
+                } else {
+                    pair := HashBstID{hash: hash, bst_id: job.bst_id}
+                    wr.ReturnQueue <- pair
+                }
                 wr.SyncWaitGroup.Done()
             case <-wr.Quit:
                 fmt.Println("worker id calling quit", wr.ID)
@@ -57,49 +77,23 @@ func (wr *Worker) Start() {
     }() 
 }
 
-func (wr *Worker) StartAndUpdateMap() {
-    go func() {
-        for {
-            wr.Queue <- wr.JobChan //worker puts its jobChan on queue (channel of channels). wr.Queue populated by dispatcher.process()
-            select {
-            case job := <-wr.JobChan:
-                var hash int = job.tree.computeHash() //todo: extract below to outside, iterate over list of trees with hash_workers
-                hash_bst_pair := HashBstID{hash: hash, bst_id: job.bst_id}
-                wr.ReturnQueue <- hash_bst_pair
-                wr.SyncWaitGroup.Done()
-            case <-wr.Quit:
-                fmt.Println("worker id calling quit", wr.ID)
-                close(wr.JobChan)
-                return
-            }
-        }
-    }() 
-}
-
-
-type disp struct {
-    Workers            []*Worker  // this is the list of workers that dispatcher tracks
-    WorkChan           JobChannel // client submits a job to this channel
-    Queue              JobQueue   // this is the shared JobPool between the workers
-    ReturnQueue        chan HashBstID
-    SyncWaitGroup      *sync.WaitGroup
-    UseWorkersForData  bool
-}
 
 
 
 func (d *disp) Start() *disp {
-    //defer d.SyncWaitGroup.Done()
     l := len(d.Workers)
-    for i := 1; i <= l; i++ {
+    for i := 0; i < l; i++ {
         wrk := &Worker{ID: i, JobChan: make(JobChannel), Queue: d.Queue, Quit: make(chan struct{}), 
-                       ReturnQueue: d.ReturnQueue, SyncWaitGroup: d.SyncWaitGroup}
+                       ReturnQueue: d.ReturnQueue, SyncWaitGroup: d.SyncWaitGroup,
+                       WorkersUpdate: d.WorkersUpdate, Mutex: d.Mutex, Bst_hashmap: d.Bst_hashmap,}
         wrk.Start()
         d.Workers = append(d.Workers, wrk)
     }
     go d.process()
     return d
 }
+
+
 
 func (d *disp) process() {
     for {
@@ -114,8 +108,7 @@ func (d *disp) process() {
 func (d *disp) Submit(job Job) {
     d.WorkChan <- job
 }
-
-
+/*****************************************************************************************/
 /*****************************************************************************************/
 
 type InputArgs struct {
@@ -124,6 +117,14 @@ type InputArgs struct {
     comp_workers *int
     input_file *string
     run_mode int
+}
+
+type WorkersArgs struct {
+    sequential              bool
+    hashwrkrs_update_map    bool
+    exit_hash               bool
+    exit_map                bool
+    extra_credit            bool
 }
 
 type HashBstID struct {
@@ -135,13 +136,6 @@ type Node struct {
     Value int
     Left *Node
     Right *Node
-}
-
-
-func checkfile(e error) {
-    if e != nil {
-        panic(e)
-    }
 }
 
 func (n *Node) computeHash() int {
@@ -231,11 +225,113 @@ func equalTrees(a *Node, b *Node) bool {
     return true
 }
 
+func compute_hash(tree *Node, bst_hashmap *map[int][]int, bst_id int, 
+                 queue chan<- HashBstID, wg *sync.WaitGroup){
+    
+    defer wg.Done()
+    
+    var hash int = tree.computeHash() //todo: extract below to outside, iterate over list of trees with hash_workers
+    hash_bst_pair := HashBstID{hash: hash, bst_id: bst_id}
+    queue <- hash_bst_pair
+}
+
+func run(bst_list *[]*Node, bst_hashmap *map[int][]int,
+         tree_equal *map[int][]int, args InputArgs, 
+         worker_args WorkersArgs, test_hash_time bool) {
+    
+    
+    var wg sync.WaitGroup
+    var mutex sync.Mutex
+    /***STEP 1***/
+    build_trees(args.input_file, bst_list) //always sequential to preserve tree ordering
+    fmt.Println("number of trees: ", len(*bst_list))
+    queue := make(chan HashBstID, len(*bst_list)) //HAS TO GO AFTER BUILD TREES!!!!
+    /***STEP 2--hash trees***/
+    if (worker_args.sequential){
+        if (worker_args.exit_map && worker_args.exit_hash){
+            for _, tree := range(*bst_list){
+                tree.computeHash()
+            }
+            return
+        } else {
+            for bst_id, tree := range(*bst_list){
+                var hash int = tree.computeHash()
+                (*bst_hashmap)[hash] = append((*bst_hashmap)[hash], bst_id)
+            }
+            compare_trees(bst_list, bst_hashmap, tree_equal)
+            return
+        }
+    }
+    if test_hash_time {
+        start := time.Now()
+        for bst_id, tree := range *bst_list{
+            wg.Add(1)
+            go compute_hash(tree, bst_hashmap, bst_id, queue, &wg)
+        }
+        wg.Wait()
+        elapsed := time.Since(start)
+        fmt.Printf("hashing took %s\n", elapsed)
+    } else {
+        wg.Add(len(*bst_list))
+        fmt.Println("hash workers update map ", worker_args.hashwrkrs_update_map)
+        dd := &disp{Workers:  make([]*Worker, *args.hash_workers),
+                    WorkChan: make(JobChannel),
+                    Queue:    make(JobQueue),
+                    ReturnQueue: queue,
+                    SyncWaitGroup: &wg,
+                    WorkersUpdate: worker_args.hashwrkrs_update_map,
+                    Mutex: &mutex,
+                    Bst_hashmap: bst_hashmap}
+        dd.Start()
+
+        start := time.Now()
+        for i, tree := range *bst_list {
+            dd.Submit(Job{ tree:tree, Name:fmt.Sprintf("JobID::%d", i), bst_id: i} )
+        }
+        wg.Wait()
+        elapsed := time.Since(start)
+        fmt.Printf("hashing took %s\n", elapsed)
+    }
+    
+    close(queue)
+    
+    if worker_args.exit_hash{
+        return
+    }
+    
+    /***STEP 3--find duplicates(data_workers)***/
+    if *args.data_workers==1{
+        for pair := range queue{
+            (*bst_hashmap)[pair.hash] = append((*bst_hashmap)[pair.hash], pair.bst_id)
+        }
+    }
+    
+    if worker_args.exit_map {
+        return
+    }
+    /***STEP 4--compare equal trees***/
+    compare_trees(bst_list, bst_hashmap, tree_equal)
+}
+
+func build_worker_args(args InputArgs) WorkersArgs {
+    exit_hash, exit_map, hashwrkrs_update_map, sequential, extra_credit := false, false, false, false, false
+
+    if *args.data_workers==0{ exit_hash = true }
+    if *args.comp_workers==0{ exit_map = true }
+    if *args.hash_workers==1{ sequential = true }
+    if !sequential && (*args.hash_workers==*args.data_workers){ hashwrkrs_update_map = true }
+    if !sequential && *args.data_workers > 1 && !hashwrkrs_update_map{ extra_credit = true }
+    
+    worker_args := WorkersArgs{exit_map: exit_map, exit_hash: exit_hash,
+                               hashwrkrs_update_map: hashwrkrs_update_map,
+                               sequential: sequential, extra_credit: extra_credit}
+    return worker_args
+}
 
 func parse_args() InputArgs {
     hash_workers := flag.Int("hash-workers", 1, "an int")
-    data_workers := flag.Int("data-workers", 1, "an int")
-    comp_workers := flag.Int("comp-workers", 1, "an int")
+    data_workers := flag.Int("data-workers", 0, "an int")
+    comp_workers := flag.Int("comp-workers", 0, "an int")
     input_file   := flag.String("input", "", "a string")
     
     flag.Parse()
@@ -247,9 +343,33 @@ func parse_args() InputArgs {
     return args
 }
 
+func main() {
+    
+    var test_hash_time bool = false //TODO: for testing purposes: test spawning one go routine per tree vs worker pool 
+    
+    var bst_list []*Node; //list of trees
+    bst_hashmap := make(map[int][]int) //hash id: list of bst_ids
+    tree_equal := make(map[int][]int) //groups of equal trees
+    
+    var args InputArgs = parse_args()
+    /*******   PRINTING INPUTS ******/
+    fmt.Println("hash workers:", *args.hash_workers, "data workers:", *args.data_workers, "comp workers:", 
+                *args.comp_workers, "input file:", args.input_file)
 
-
-
+    worker_args := build_worker_args(args)
+    run(&bst_list, &bst_hashmap, &tree_equal, args, worker_args, test_hash_time)
+    
+    //fmt.Println(bst_hashmap)
+    //fmt.Println(bst_list)
+    //fmt.Println(tree_equal)
+    for key,value := range tree_equal {
+        if len(value) <= 1 {
+            delete(tree_equal, key)
+        }
+    }
+    fmt.Println(tree_equal)
+    
+}
 
 func compare_trees(bst_list *[]*Node, bst_hashmap *map[int][]int,
                    tree_equal *map[int][]int){
@@ -259,7 +379,7 @@ func compare_trees(bst_list *[]*Node, bst_hashmap *map[int][]int,
     for hash, bstids := range *bst_hashmap {
         this_group_visited := make(map[int]bool)
         if (len(bstids) > 1) {
-            fmt.Printf("Compare values in key: %d\n", hash)
+            //fmt.Printf("Compare values in key: %d\n", hash)
             for i:=0; i < len(bstids); i++ {
                 if (!this_group_visited[i]){
                     
@@ -285,89 +405,4 @@ func compare_trees(bst_list *[]*Node, bst_hashmap *map[int][]int,
             }
         }// else no need to print groups with only one tree
     }
-}
-
-func compute_hash(tree *Node, bst_hashmap *map[int][]int, bst_id int, 
-                 queue chan<- HashBstID, wg *sync.WaitGroup){
-    
-    defer wg.Done()
-    
-    var hash int = tree.computeHash() //todo: extract below to outside, iterate over list of trees with hash_workers
-    hash_bst_pair := HashBstID{hash: hash, bst_id: bst_id}
-    queue <- hash_bst_pair
-}
-
-func run_sequential(bst_list *[]*Node, bst_hashmap *map[int][]int,
-                   tree_equal *map[int][]int, args InputArgs){
-    
-    build_trees(args.input_file, bst_list)
-    
-    queue := make(chan HashBstID, len(*bst_list))
-    
-    /*
-    var wg sync.WaitGroup
-    var bst_id int = 0
-    for _, tree := range *bst_list{
-        wg.Add(1)
-        go compute_hash(tree, bst_hashmap, bst_id, queue, &wg)
-        bst_id++
-    }
-    wg.Wait()
-    close(queue)
-    for pair := range queue{
-        (*bst_hashmap)[pair.hash] = append((*bst_hashmap)[pair.hash], pair.bst_id)
-    }*/
-    
-    var wg sync.WaitGroup
-    wg.Add(len(*bst_list))
-    dd := &disp{ Workers:  make([]*Worker, *args.hash_workers),
-                 WorkChan: make(JobChannel),
-                 Queue:    make(JobQueue),
-                 ReturnQueue: queue,
-                 SyncWaitGroup: &wg,
-               }
-    dd.Start()
-    for i, tree := range *bst_list {
-        dd.Submit(Job{ tree:tree, Name:fmt.Sprintf("JobID::%d", i), bst_id: i} )
-    }
-    //close(dd.WorkChan)
-    wg.Wait()
-    close(queue)
-    for pair := range queue{
-        (*bst_hashmap)[pair.hash] = append((*bst_hashmap)[pair.hash], pair.bst_id)
-    }
-    compare_trees(bst_list, bst_hashmap, tree_equal)
-    //iterate over hashmaps, if key > 1, compare trees inside
-}
-
-
-func main() {
-    
-    bst_hashmap := make(map[int][]int)
-    var bst_list []*Node;
-    tree_equal := make(map[int][]int)
-    
-    
-    fmt.Println("Running go BST sequential")
-    
-    /*******   PARSING ARGUMENTS   *******/ 
-    var args InputArgs = parse_args()
-    
-    fmt.Println("hash workers:", args.hash_workers)
-    fmt.Println("data workers:", args.data_workers)
-    fmt.Println("comp workers:", args.comp_workers)
-    fmt.Println("input file:", args.input_file)
-    
-    run_sequential(&bst_list, &bst_hashmap, &tree_equal, args)
-    fmt.Println(bst_hashmap)
-    fmt.Println(bst_list)
-    fmt.Println(tree_equal)
-    
-    //code diagram
-    /*
-    1) read file--> if not end of line, insert into binary tree, compute hash, store hashMapBST: list.append(bst id)
-    2) for each key in hashMapBST: compare equality, separate out into linkage somehow
-    
-    
-    */
 }
